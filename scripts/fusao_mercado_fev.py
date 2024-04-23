4aa26da611cb72018c29406e1b0341a7d863f427
Maria Eduarda <mota.maeduarda@gmail.com>
2024-04-23 18:49:52 -0300

    Subindo as classes da pipelina e refatorando o código

--- scripts/fusao_mercado_fev.py
+++ scripts/fusao_mercado_fev.py
@@ -1,83 +1,21 @@
-import json 
-import csv
+from processamento_dados import Dados
 
 
-def leitura_json (path_json):
-    with open(path_json, 'r') as file:
-        dados_json = json.load(file)
-    return dados_json
-    
-def leitura_csv(path_csv):
-    dados_csv = []
-    with open(path_csv,'r') as file:
-        spamreader = csv.DictReader(file,delimiter=',')
-        for row in spamreader:
-            dados_csv.append(row)
-    return dados_csv
-
-def leitura_dados(path,tipo_arquivo):
-    dados = []
-
-    if tipo_arquivo == 'csv':
-        dados = leitura_csv(path)
-
-    elif tipo_arquivo == 'json':
-        dados = leitura_json(path)
-
-    return dados
-
-def get_columns(dados):
-    return list(dados[-1].keys())
-
-def rename_columns(dados,key_mapping):
-    new_dados_csv = []
-    for old_dict in dados:
-        dict_temp ={}
-        for old_key,value in old_dict.items():
-            dict_temp[key_mapping[old_key]]=value
-        new_dados_csv.append(dict_temp)
-    return new_dados_csv
-
-def size_data(dados):
-    return len(dados)
-
-def join (dadosA, dadosB):
-    combined_list = []
-    combined_list.extend(dadosA)
-    combined_list.extend(dadosB)
-    return combined_list
-
-def transformando_tabela(dados,nome_colunas):
-    dados_combinados_tabela = [nome_colunas]
-    for row in dados:
-        linha = []
-        for coluna in nome_colunas:
-            linha.append(row.get(coluna,'Indisponivel'))
-        dados_combinados_tabela.append(linha)
-    return dados_combinados_tabela
-
-def salvando_dados (dados,path):
-    with open(path, 'w') as file:
-        writer = csv.writer(file)
-        writer.writerows(dados)
-
-path_json = 'data_raw/dados_empresaA.json'
-path_csv = 'data_raw/dados_empresaB.csv'
-path_dados_combinados = 'data_processed/dados_combinados.csv'
-#Leitura dos dados
+path_json = 'Documentos/pipeline_dados/data_raw/dados_empresaA.json'
+path_csv = 'Documentos/pipeline_dados/data_raw/dados_empresaB.csv'
+path_dados_combinados = 'Documentos/pipeline_dados/data_processed/dados_combinados.csv'
 
 
-dados_json = leitura_dados(path_json,'json')
-nome_colunas_json = get_columns(dados_json)
-print(nome_colunas_json)
+# Extract
 
+dados_empresaA = Dados(path_json,'json')
+print(dados_empresaA.nome_colunas)
 
-dados_csv = leitura_dados(path_csv,'csv')
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_empresaB = Dados(path_csv,'csv')
+print(dados_empresaB.nome_colunas)
+print(dados_empresaB.qtd_linhas)
 
-
-#Transformação dos dados
+# Transform
 
 key_mapping = {'Nome do Item': 'Nome do Produto',
                'Classificação do Produto' :'Categoria do Produto',
@@ -86,19 +24,16 @@ key_mapping = {'Nome do Item': 'Nome do Produto',
                'Nome da Loja':'Filial',
                'Data da Venda':'Data da Venda'}
 
-dados_csv = rename_columns(dados_csv,key_mapping)
-nome_colunas_csv = get_columns(dados_csv)
-print(nome_colunas_csv)
+dados_empresaB.rename_columns(key_mapping)
+print(dados_empresaB.nome_colunas)
 
-dados_fusao = join(dados_json,dados_csv)
-nomes_colunas_fusao = get_columns(dados_fusao)
-print(nomes_colunas_fusao)
+dados_fusao = Dados.join(dados_empresaA,dados_empresaB)
+print(dados_fusao.nome_colunas)
+print(dados_fusao.qtd_linhas)
 
-#Salvando os dados
+#Load
 
-dados_fusao_tabela = transformando_tabela(dados_fusao,nomes_colunas_fusao)
-
-path_dados_combinados = 'data_processed/dados_combinados.csv'
-
-salvando_dados(dados_fusao_tabela,path_dados_combinados)
+path_dados_combinados = 'Documentos/pipeline_dados/data_processed/dados_combinados.csv'
+dados_fusao.salvando_dados(path_dados_combinados)
 print(path_dados_combinados)
+
